865b474181e2460ede490ef9297b3e732c92908b
Ume-6666 <164321141+Ume-6666@users.noreply.github.com>
2025-02-26 23:40:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const API_Key = "e1453716ba89f11ac1b3beed00fcc6f7"
+const city = "Bhopal";
+let latitude, longitude;
+const geoURL = "https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid={API_key}"
